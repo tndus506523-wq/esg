bd8ac53a172e7b0855125f55517eff869a97b831
tndus506523-wq <tndus506523@gmail.com>
2026-07-16 20:01:37 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성에서 복사하세요.
 // 아래 YOUR_... 값을 실제 값으로 교체해야 여러 휴대폰에서 실시간으로 동기화됩니다.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD5VOUM8zOSbuVVQXOjfGfNuJ9IVpAl28g",
+  authDomain: "esgcamp-86b62.firebaseapp.com",
+  projectId: "esgcamp-86b62",
+  storageBucket: "esgcamp-86b62.firebasestorage.app",
+  messagingSenderId: "786264065087",
+  appId: "1:786264065087:web:3484d447b1ccb096c9648a"
 };
 
 // 6명이 같은 방 데이터를 사용하도록 유지합니다.
